121068b35cccc731074f3d807cdcb88cbc248257
Van Viet Hoang <93271681+jsthoang@users.noreply.github.com>
2022-11-21 15:14:51 +0700

    asfasf

--- routes/list_staff.js
+++ routes/list_staff.js
@@ -2,11 +2,7 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const listStaff_json = require("../lixi/listStaff.json");
-router.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
 router.get("/", (req, res) => {
     res.send(JSON.stringify(listStaff_json));
 });
