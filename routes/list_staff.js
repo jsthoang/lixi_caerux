08feb46579c90501016151e2bf7041d02662a1ab
Van Viet Hoang <93271681+jsthoang@users.noreply.github.com>
2022-11-21 15:06:09 +0700

    Update list_staff.js

--- routes/list_staff.js
+++ routes/list_staff.js
@@ -4,11 +4,9 @@ const fs = require("fs");
 const listStaff_json = require("../lixi/listStaff.json");
 
 router.get("/", (req, res) => {
-    console.log("ehhe");
     res.send(JSON.stringify(listStaff_json));
 });
 router.get("/:id", (req, res) => {
-    console.log("ehhe");
     const userId = req.params["id"];
     res.send(JSON.stringify(listStaff_json[userId]));
 });
@@ -16,20 +14,16 @@ router.use(express.json()); // for parsing application/json
 router.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 router.post("/", (req, res) => {
-    console.log("/post done");
-    console.log(req.body);
     fs.writeFile("./lixi/listStaff.json", JSON.stringify(req.body), (err) => {
         if (err) console.log(err);
         else {
             console.log("File written successfully");
         }
     });
-    res.send("Post done");
 });
 router.put("/:id", (req, res) => {
     const userId = req.params["id"];
     listStaff_json[userId] = req.body;
-    console.log(listStaff_json);
     fs.writeFile("./lixi/listStaff.json", JSON.stringify(listStaff_json), (err) => {
         if (err) console.log(err);
         else {
