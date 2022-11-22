4069c62aa0fd06b620091fd9dd40c965d2d2e88c
Van Viet Hoang <93271681+jsthoang@users.noreply.github.com>
2022-11-22 08:34:12 +0700

    add js to gift and prize

--- routes/gift.js
+++ routes/gift.js
@@ -1,9 +1,38 @@
 const express = require("express");
 const router = express.Router();
-
+const fs = require("fs");
 const gift_json = require("../lixi/gift.json");
 
 router.get("/", (req, res) => {
-    console.log("gift-json is oke");
-    router.send(JSON.stringify(gift_json));
+    res.send(JSON.stringify(gift_json));
 });
+router.get("/:id", (req, res) => {
+    const userId = req.params["id"];
+    res.send(JSON.stringify(gift_json[userId]));
+});
+router.use(express.json()); // for parsing application/json
+router.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+router.post("/", (req, res) => {
+    fs.writeFile("./lixi/gift.json", JSON.stringify(req.body), (err) => {
+        if (err) console.log(err);
+        else {
+            console.log("File written successfully");
+        }
+    });
+    res.send({ Status: "Ban da POST thanh cong" });
+});
+
+router.put("/:id", (req, res) => {
+    const userId = req.params["id"];
+    gift_json[userId] = req.body;
+    fs.writeFile("./lixi/gift.json", JSON.stringify(gift_json), (err) => {
+        if (err) console.log(err);
+        else {
+            console.log("File written successfully");
+        }
+    });
+    res.send({ Status: "Ban da PUT thanh cong" });
+});
+
+module.exports = router;
--- routes/prize.js
+++ routes/prize.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const router = express.Router();
-
+const fs = require("fs");
 const prize_json = require("../lixi/prize.json");
 
-router.get("/", (red, res) => {
-    console.log("prize-ok");
+router.get("/", (req, res) => {
     res.send(JSON.stringify(prize_json));
+});
+router.get("/:id", (req, res) => {
+    const userId = req.params["id"];
+    res.send(JSON.stringify(prize_json[userId]));
+});
+router.use(express.json()); // for parsing application/json
+router.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+router.post("/", (req, res) => {
+    fs.writeFile("./lixi/prize.json", JSON.stringify(req.body), (err) => {
+        if (err) console.log(err);
+        else {
+            console.log("File written successfully");
+        }
+    });
+    res.send({ Status: "Ban da POST thanh cong" });
+});
+
+router.put("/:id", (req, res) => {
+    const userId = req.params["id"];
+    prize_json[userId] = req.body;
+    fs.writeFile("./lixi/prize.json", JSON.stringify(prize_json), (err) => {
+        if (err) console.log(err);
+        else {
+            console.log("File written successfully");
+        }
+    });
+    res.send({ Status: "Ban da PUT thanh cong" });
 });
 
 module.exports = router;
