f4764841ce93090d9ee619d1ba67bde50a4395ab
Van Viet Hoang <93271681+jsthoang@users.noreply.github.com>
2023-01-16 14:54:05 +0700

    remove cor header

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.post("/", (req, res) => {
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
     next();
 });
